33d8a3beef9ebbd80c402ab4c86a3293c247043b
Luka Warren <lukawarren1@gmail.com>
2024-07-02 19:57:19 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include <adwaita.h>
+#include <glib/gstdio.h>
+
+static void on_click(GtkButton* button)
+{
+    (void)button;
+    printf("Hello world!\n");
+}
+
+static void on_activate(GtkApplication *app)
+{
+    GtkBuilder* builder = gtk_builder_new_from_resource("/com/github/lukawarren/waveform/src/ui/window.ui");
+
+    GObject* window = gtk_builder_get_object(builder, "window");
+    gtk_window_set_application(GTK_WINDOW(window), app);
+
+    gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
+    g_object_unref(builder);
+}
+
+int main(int argc, char** argv)
+{
+    g_autoptr(AdwApplication) app = NULL;
+    app = adw_application_new("org.example.luka", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+    return g_application_run(G_APPLICATION(app), argc, argv);
+}
