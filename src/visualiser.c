65f5bdf6e15f5f3b43981c228eab014b2a4dd10f
Luka Warren <lukawarren1@gmail.com>
2024-07-05 13:24:41 +0100

    Better drawing

--- src/visualiser.c
+++ src/visualiser.c
@@ -1,46 +1,98 @@
 #include "visualiser.h"
 
-static float* audio_data;
+#define N_FRAMES 10
+
+static float* audio_data[N_FRAMES] = {};
 static int audio_length;
+static int current_frame = 0;
+
+static int get_occupied_frames()
+{
+    for (int i = 0; i < N_FRAMES; ++i)
+        if (audio_data[i] == NULL)
+            return i;
+
+    return N_FRAMES;
+}
+
+static float get_average_frame_value(int index)
+{
+    int frames = get_occupied_frames();
+    float sum = 0.0f;
+    for (int i = 0; i < frames; ++i)
+    {
+        // If we average an entire song we would expect the result to be
+        // about zero, so it'll look visually better if we abs everything
+        float value = audio_data[i][index];
+        sum += fabs(value);
+    }
+    return sum / (float)frames;
+}
 
 void visualiser_draw_function(
     GtkDrawingArea* area,
     cairo_t*        cairo,
     int             width,
     int             height,
-    gpointer        data
+    gpointer
 )
 {
-    if (audio_data == NULL)
+    if (get_occupied_frames() == 0)
         return;
 
-    GdkRGBA bar_colour;
-    gtk_widget_get_color(GTK_WIDGET(area), &bar_colour);
-    gdk_cairo_set_source_rgba(cairo, &bar_colour);
+    int step_size = 5;
+    float scale = 10.0f;
 
-    for (int i = 0; i < width; ++i)
+    for (int i = 0; i < width; i += step_size)
     {
-        int progress = (int)((float)i / (float)width * (float)audio_length);
+        // Position in audio
+        float f = (float)i / (float)width;
+        int progress = (int)(f * (float)audio_length);
         progress = MIN(MAX(progress, 0), audio_length - 1);
-        float bar_height = (audio_data[progress] + 1.0f) / 2.0f * (float)height;
 
+        // Bar height
+        float bar_height = get_average_frame_value(progress);
+        bar_height = bar_height / 2.0f * (float)height;
+        bar_height *= scale;
+
+        // Colour
+        GdkRGBA colour;
+        colour.alpha = 1.0f;
+        colour.red = colour.blue = colour.green = 1.0f - sinf(G_PI * f);
+        gdk_cairo_set_source_rgba(cairo, &colour);
         cairo_rectangle(cairo, i, height - bar_height - 1.0f, 1.0f, bar_height);
+        cairo_fill(cairo);
     }
-
-
-    cairo_fill(cairo);
 }
 
 void visualiser_set_data(AudioPacket* packet)
 {
-    visualiser_free_data();
-    audio_data = malloc(sizeof(packet->data[0]) * packet->length);
-    memcpy(audio_data, packet->data, sizeof(packet->data[0]) * packet->length);
-    audio_length = packet->length;
+    // Delete old frame
+    if (audio_data[current_frame] != NULL)
+        free(audio_data[current_frame]);
+
+    int channels = 2;
+    audio_data[current_frame] = malloc(sizeof(packet->data[0]) * packet->length / channels);
+
+    // Average over both channels
+    for (int i = 0; i < packet->length / channels; i++)
+        audio_data[current_frame][i] = (packet->data[i * 2] + packet->data[i * 2 + 1]) / 2.0f;
+
+    // Each packet should have an identical length
+    // so there is no need to check they all match
+    audio_length = packet->length / channels;
+
+    current_frame = (current_frame + 1) % N_FRAMES;
 }
 
 void visualiser_free_data()
 {
-    if (audio_data != NULL)
-        free(audio_data);
+    for (int i = 0; i < N_FRAMES; ++i)
+    {
+        if (audio_data[i] != NULL)
+        {
+            free(audio_data[i]);
+            audio_data[i] = NULL;
+        }
+    }
 }
