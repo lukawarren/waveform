d275b378365376b4e7e50462ea0360c0043a26a9
Luka Warren <lukawarren1@gmail.com>
2024-07-06 13:09:01 +0100

    Added reset preferences button

--- src/preferences.c
+++ src/preferences.c
@@ -3,6 +3,9 @@
 #include <adwaita.h>
 
 static GSettings* settings;
+static GObject* window;
+
+static void on_reset_preferences();
 
 void init_preferences()
 {
@@ -12,7 +15,7 @@ void init_preferences()
 void create_preferences_window()
 {
     GtkBuilder* builder = gtk_builder_new_from_resource("/com/github/lukawarren/waveform/src/ui/preferences.ui");
-    GObject* window = gtk_builder_get_object(builder, "preferences_window");
+    window = gtk_builder_get_object(builder, "preferences_window");
     gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
 
     GtkWidget* visualisation_type   = GET_WIDGET("visualisation_type");
@@ -20,6 +23,7 @@ void create_preferences_window()
     GtkWidget* minimum_frequency    = GET_WIDGET("minimum_frequency");
     GtkWidget* maximum_frequency    = GET_WIDGET("maximum_frequency");
     GtkWidget* use_bark_scale       = GET_WIDGET("use_bark_scale");
+    GtkWidget* reset_button         = GET_WIDGET("reset_button");
 
     g_settings_bind(
         settings,
@@ -61,6 +65,8 @@ void create_preferences_window()
         G_SETTINGS_BIND_DEFAULT
     );
 
+    g_signal_connect(reset_button, "clicked", G_CALLBACK(on_reset_preferences), NULL);
+
     g_object_unref(builder);
 }
 
@@ -95,4 +101,45 @@ int preferences_get_maximum_frequency()
 bool preferences_get_use_bark_scale()
 {
     return g_settings_get_boolean(settings, "use-bark-scale");
+}
+
+static void on_reset_confirmed(GObject* self, GAsyncResult* result, gpointer)
+{
+    int index = gtk_alert_dialog_choose_finish(
+        GTK_ALERT_DIALOG(self),
+        result,
+        NULL
+    );
+
+    if (index == 1)
+    {
+        g_settings_reset(settings, "gap-size");
+        g_settings_reset(settings, "visualisation-type");
+        g_settings_reset(settings, "minimum-frequency");
+        g_settings_reset(settings, "maximum-frequency");
+        g_settings_reset(settings, "use-bark-scale");
+    }
+}
+
+static void on_reset_preferences()
+{
+    GtkAlertDialog* dialog = gtk_alert_dialog_new("Are you sure you want to reset all preferences?");
+
+    const char* const buttons[] = {
+        "No",
+        "Yes",
+        NULL
+    };
+
+    gtk_alert_dialog_set_buttons(dialog, buttons);
+    gtk_alert_dialog_set_default_button(dialog, 0);
+
+    gtk_alert_dialog_choose(
+        dialog,
+        GTK_WINDOW(window),
+        NULL,
+        on_reset_confirmed,
+        NULL
+    );
+    g_object_unref(dialog);
 }
